8690eba54757a50377eead0b881bc6433d22ade9
Geoff Matis <gmatis@gmail.com>
2021-01-10 15:48:03 -0500

    dropping drop table statements and adding FK to employees

--- table_schema.sql
+++ table_schema.sql
@@ -1,7 +1,5 @@
 -- create departments table
 
-DROP TABLE departments; 
-
 CREATE TABLE departments(
 	dept_no VARCHAR(30) NOT NULL, 
 	dept_name VARCHAR(30) NOT NUll,
@@ -11,8 +9,8 @@ CREATE TABLE departments(
 select * from departments;
 
 -- create employees table (has to go before dept_emp so it can reference the PK)
+-- titles had to be created before employees so employees could have FK title_id
 
-DROP TABLE employees;
 
 CREATE TABLE employees(
 	emp_no INT NOT NULL, 
@@ -22,15 +20,13 @@ CREATE TABLE employees(
 	last_name VARCHAR(30) NOT NULL,
 	sex VARCHAR(7) NOT NULL,
 	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
-	--would like to make (title_id a foreign key but can't figure it out as of yet)
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 select * from employees;
 
 -- create dept_emp table 
-
-DROP TABLE dept_emp;
 
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
@@ -43,8 +39,6 @@ select * from dept_emp;
 
 -- create dept_manager table
 
-DROP TABLE dept_manager;
-
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(30) NOT NULL,
 	emp_no INT NOT NULL,
@@ -56,7 +50,6 @@ select * from dept_manager;
 
 -- create salaries table
 
-DROP TABLE salaries;
 
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
@@ -67,8 +60,6 @@ CREATE TABLE salaries(
 select * from salaries;
 
 -- create titles table
-
-DROP TABLE titles;
 
 CREATE TABLE titles(
 	title_id VARCHAR(30) NOT NULL,
